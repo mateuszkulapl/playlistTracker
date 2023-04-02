ddbeefee3f3d6d2e5077178ea3e79bbc87034708
mateuszkulapl <mateuszkulapl@gmail.com>
2023-04-02 13:27:54 +0200

    static fiels version

--- resources/views/components/layout/index.blade.php
+++ resources/views/components/layout/index.blade.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{config('app.name') }}</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link href="{{ asset('css/app.css') }}?v=1.1.0" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}?v=1.1.0" defer></script>
     @livewireStyles
 </head>
 
