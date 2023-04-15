f2e6c966181b37ec7b135f49dda2392b328dc8bb
mateuszkulapl <mateuszkulapl@gmail.com>
2023-04-15 21:06:08 +0200

    fix images

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -3,7 +3,7 @@
     <div class="max-w-7xl w-full mx-auto px-2 py-2 flex justify-between">
         <div class="">
             <a class="text-xl gap-x-2 flex items-center" href="{{ route('playlist.index') }}">
-                <img src="/img/icon.png" alt="icon" class="h-10 w-10">
+                <img src="/img/icon.png?v=2" alt="icon" class="h-10 w-10">
                 <span>{{ config('app.name') }}</span>
             </a>
         </div>
