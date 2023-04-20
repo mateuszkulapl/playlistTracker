4707000bb765f534dba5d44d9a90f82a7a2c7a2f
mateuszkulapl <mateuszkulapl@gmail.com>
2023-04-20 19:24:25 +0000

    add scroll to top button

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
-require('./bootstrap');
+// require('./bootstrap');
+
+var toTopButton = document.getElementById("scrollTop"); 
+toTopButton.addEventListener("click", goToTop);
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        toTopButton.classList.remove("hidden");
+    } else {
+        toTopButton.classList.add("hidden");
+    }
+}
+function goToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
--- resources/views/components/layout/index.blade.php
+++ resources/views/components/layout/index.blade.php
@@ -1,24 +1,27 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="scroll-smooth ">
+<html class="scroll-smooth " lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{config('app.name') }}</title>
-    <link href="{{ asset('css/app.css') }}?v=1.5.0" rel="stylesheet">
-    <script src="{{ asset('js/app.js') }}?v=1.5.0" defer></script>
-    <link rel="stylesheet" href="/favicon.ico">
+    <title>{{ config('app.name') }}</title>
+    <link href="{{ asset('css/app.css') }}?v=1.5.6" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}?v=1.5.6" defer></script>
+    <link href="/favicon.ico" rel="stylesheet">
     @livewireStyles
 </head>
 
 <body class="font-sans text-gray-700 bg-slate-300 black flex flex-col min-h-screen">
-        <x-layout.header />
+    <x-layout.header />
     <x-layout.main>
         {{ $slot }}
+        <button class="hidden fixed z-90 bottom-8 right-8 border-1 w-12 h-12 rounded-full drop-shadow-md text-3xl font-black" id="scrollTop" title="Wróć na górę strony" onclick="goToTop()">&uarr;</button>
     </x-layout.main>
     <x-layout.footer class="">
         <p>&copy; {{ config('app.name') }}</p>
     </x-layout.footer>
     @livewireScripts
 </body>
+
 </html>
