7f332fb0663892a40206513bba0a1c574f50df13
mateuszkulapl <mateuszkulapl@gmail.com>
2023-08-16 21:54:28 +0200

    test blocks visibility

--- tests/Feature/ViewTest.php
+++ tests/Feature/ViewTest.php
@@ -2,28 +2,44 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ViewTest extends TestCase
 {
-    use RefreshDatabase;
+    use DatabaseTransactions;
 
     public function test_add_category_button_is_visible(): void
     {
         $response = $this->get('/');
-        $response->assertSee('Dodaj kategorię');
+        $response->assertSeeText('Dodaj kategorię');
     }
+
     public function test_add_tag_input_is_visible(): void
     {
         $this->seed();
         $response = $this->get('/');
         $response->assertSee('Nazwa tagu');
     }
+
     public function test_import_block_title_is_visible(): void
     {
         $this->seed();
         $response = $this->get('/');
-        $response->assertSee('Importuj do kategorii ');
+        $response->assertSeeText('Importuj do kategorii ');
+    }
+
+    public function test_deleted_block_title_is_visible(): void
+    {
+        $this->seed();
+        $response = $this->get('/');
+        $response->assertSeeText('Usunięte elementy');
+    }
+
+    public function test_edit_awards_title_is_visible(): void
+    {
+        $this->seed();
+        $response = $this->get('/');
+        $response->assertSeeText('Edytuj nagrody');
     }
 }
