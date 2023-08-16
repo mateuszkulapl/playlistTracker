b2a1d2c92db0b941ff593af1b8001083306554bf
mateuszkulapl <mateuszkulapl@gmail.com>
2023-08-16 22:00:12 +0200

    remove table def

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -10,69 +10,16 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class TagFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-
-    /*
-     [
-    {#6481
-      +"Field": "id",
-      +"Type": "bigint(20) unsigned",
-      +"Null": "NO",
-      +"Key": "PRI",
-      +"Default": null,
-      +"Extra": "auto_increment",
-    },
-    {#6483
-      +"Field": "name",
-      +"Type": "varchar(255)",
-      +"Null": "NO",
-      +"Key": "UNI",
-      +"Default": null,
-      +"Extra": "",
-    },
-    {#6484
-      +"Field": "created_at",
-      +"Type": "timestamp",
-      +"Null": "YES",
-      +"Key": "",
-      +"Default": null,
-      +"Extra": "",
-    },
-    {#6485
-      +"Field": "updated_at",
-      +"Type": "timestamp",
-      +"Null": "YES",
-      +"Key": "",
-      +"Default": null,
-      +"Extra": "",
-    },
-    {#6486
-      +"Field": "deleted_at",
-      +"Type": "timestamp",
-      +"Null": "YES",
-      +"Key": "",
-      +"Default": null,
-      +"Extra": "",
-    },
-    {#6487
-      +"Field": "category_id",
-      +"Type": "bigint(20) unsigned",
-      +"Null": "YES",
-      +"Key": "",
-      +"Default": null,
-      +"Extra": "",
-    },
-  ]
-  */
-    public function definition(): array
-    {
-        return [
-            'name' => $this->faker->word(),
-            'category_id' => Category::factory(),
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      'name' => $this->faker->word(),
+      'category_id' => Category::factory(),
+    ];
+  }
 }
