d2c8dd0a844e28f29c3248ae6dc8a26aaf55a6d3
mateuszkulapl <mateuszkulapl@gmail.com>
2023-09-03 18:23:10 +0000

    add cookie with last category

--- public/js/app-document-end.js
+++ public/js/app-document-end.js
@@ -1,17 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!******************************************!*\
-  !*** ./resources/js/app-document-end.js ***!
-  \******************************************/
-Livewire.on('toast', function (msg) {
-  var toast = document.getElementById('toast-template').content.cloneNode(true);
-  var randomIdString = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-  toast.firstElementChild.id = randomIdString;
-  toast.querySelector('.toast-msg').innerText = msg;
-  document.getElementById('toasts').appendChild(toast);
-  setTimeout(function () {
-    document.getElementById(randomIdString).remove();
-  }, 3000);
-});
-/******/ })()
-;
+Livewire.on("toast",(function(e){var t=document.getElementById("toast-template").content.cloneNode(!0),n=Math.random().toString(36).substring(2,15)+Math.random().toString(36).substring(2,15);t.firstElementChild.id=n,t.querySelector(".toast-msg").innerText=e,document.getElementById("toasts").appendChild(t),setTimeout((function(){document.getElementById(n).remove()}),3e3)})),Livewire.on("categorySelected",(function(e){!function(e,t,n){var o=new Date;o.setTime(o.getTime()+24*n*60*60*1e3);var i="expires="+o.toUTCString();document.cookie=e+"="+t+";"+i+";path=/"}("categoryId",e,e)}));
